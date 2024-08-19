eb27ead675fe8071b0e3b5c47486084dddfd596e
Oleg Benets <oleg.benets@web.de>
2024-08-19 22:34:22 +0200

    added stock-service; components; font

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <div id="app">
-
+  <header>
+    <img src="./assets/img/Rectangle 1.png" alt="rectangle image">
+    <h1>The Magnificent Seven Companies</h1>
+  </header>
+  <NavBarCard></NavBarCard>
+  <div class="big-cards-container">
+    <CustomCard></CustomCard>
+    <CustomCard></CustomCard>
   </div>
 </template>
 
 <script>
+  import CustomCard from "./components/CustomCard.vue"
+  import NavBarCard from "./components/NavBar.vue";
+  import { stockService } from "./services/stock-service";
+  import './main.css';
 
 export default {
   name: 'App',
   components: {
+    CustomCard,
+    NavBarCard,
+  },
+
+  data() {
+    return {
+      companies: ['AAPL', 'MSFT', 'TSLA', 'NVDA', 'AMZN', 'META', 'GOOG'],
+      companyData: [],
+    }
+  },
+
+  async created() {
+    const promises = this.companies.map(async (ticker) => {
+    const data = await stockService.getRevenue(`$${ticker}`);
+      console.log( ticker, data);
+    });
+    this.companyData = await Promise.all(promises);
+    console.log(this.companyData);
+    
   }
 }
+
 </script>
 
 <style>
 body {
   margin: 0;
+  overflow-x: hidden;
+}
+
+header {
+  position: absolute;
+  top: 64px;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 24px;
+}
+
+h1 {
+  font-size: 36px;
+  font-weight: 500;
+  color: #F9F9F9;
+}
+
+.big-cards-container {
+  display: flex;
+  position: absolute;
+  top: 380px;
+  left: 100px;
 }
 
 #app {
-  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+  box-sizing: border-box;
   width: 100vw;
   min-height: 100vh;
+  padding: 100px;
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav class="navbar">
+        <div class="single-company"></div>
+        <div class="single-company"></div>
+        <div class="single-company"></div>
+        <div class="single-company"></div>
+        <div class="single-company"></div>
+        <div class="single-company"></div>
+        <div class="single-company"></div>
+    </nav>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'NavBarCard',
+  }
+  </script>
+  
+  <style scoped>
+  .navbar {
+    position: absolute;
+    top: 155px;
+    left: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    width: calc(90vw - 48px);
+    height: calc(200px - 48px);
+    border-radius: 20px;
+    padding: 24px;
+    background-color: #023A6233;
+  }
+
+  .single-company {
+    width: calc(173px - 48px);
+    height: calc(143px - 40px);
+    border-radius: 16px;
+    padding: 20px 24px;
+    background-color: #011F35;
+  }
+  </style>
